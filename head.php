7466dc790147414495c45d1abc09f482ac6ebc1e
iegarcia <nachogarcia003@gmail.com>
2019-06-21 13:04:50 -0300

    SIPBA

---
+++ head.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="Health medical template project">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/db2580de36.js"></script>
+  <link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+  <link rel="stylesheet" type="text/css" href="styles/responsive.css">
+</head>
+
+<!-- <div class="menu trans_500">
+<div class="menu_content d-flex flex-column align-items-center justify-content-center text-center">
+<div class="menu_close_container">
+<div class="menu_close">
+</div>
+</div>
+<ul>
+<li class="menu_item"><a href="index.php">Home</a></li>
+<li class="menu_item"><a href="#">Quienes Somos</a></li>
+<li class="menu_item"><a href="#">Servicios</a></li>
+<li class="menu_item"><a href="news.php">News</a></li>
+<li class="menu_item"><a href="contacto.php">Contacto</a></li>
+</ul>
+</div>
+</div> -->
+
+<header class="header" id="header">
+  <div>
+    <div class="header_top">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="header_top_content d-flex flex-row align-items-center justify-content-start">
+              <div class="logo">
+                <a href="index.php">logo</a>
+              </div>
+              <div class="header_top_extra d-flex flex-row align-items-center justify-content-start ml-auto">
+                <div class="header_top_nav">
+                  <ul class="d-flex flex-row align-items-center justify-content-start">
+                    <li><a href="#contacto">Contacto</a></li>
+                    <li><a href="#ubicacion">Donde Estamos</a></li>
+                    <!-- <li><a href="#">Appointment</a></li> -->
+                  </ul>
+                </div>
+                <div class="header_top_phone">
+                  <i class="fa fa-phone" aria-hidden="true"></i>
+                  <span>+34 586 778 8892</span>
+                </div>
+              </div>
+              <div class="hamburger ml-auto"><i class="fa fa-bars" aria-hidden="true"></i></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  </div>
+</header>
---
+++ index.php
@@ -0,0 +1,146 @@
+<?php
+require_once("head.php");
+?>
+
+<body>
+	<div class="super_container">
+		<div class="home">
+			<div class="background_image" style="background-color:grey;"></div>
+
+			<div class="home_container">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<div class="home_content">
+								<div class="home_title">Banner Principal (Opcional)</div>
+								<!-- <div class="home_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien.</div> -->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Info Boxes -->
+
+		<div class="info">
+			<!-- <div class="container">
+			<div class="row row-eq-height">
+
+
+			<div class="col-lg-4 info_box_col">
+			<div class="info_box">
+			<div class="info_image"><img src="images/info_1.jpg" alt=""></div>
+			<div class="info_content">
+			<div class="info_title">Free Consultations</div>
+			<div class="info_text">Arcu neque, scelerisque eget ligula ac, congue tempor felis. Integer tempor, eros a egestas finibus, dolor risus mollis.</div>
+			<div class="button info_button"><a href="#"><span>read more</span><span>read more</span></a></div>
+		</div>
+	</div>
+</div>
+
+
+<div class="col-lg-4 info_box_col">
+<div class="info_box">
+<div class="info_image"><img src="images/info_2.jpg" alt=""></div>
+<div class="info_content">
+<div class="info_title">Emergency Care</div>
+<div class="info_text">Morbi arcu neque, scelerisque eget ligula ac, congue tempor felis. Integer tempor, eros a egestas finibus, dolor risus.</div>
+<div class="button info_button"><a href="#"><span>read more</span><span>read more</span></a></div>
+</div>
+</div>
+</div>
+</div>
+</div> -->
+</div>
+
+<!-- Services -->
+
+<div class="services">
+	<div class="container">
+		<div class="row">
+			<div class="col text-center">
+				<div class="section_title">Our Services</div>
+				<div class="section_subtitle">to choose from</div>
+			</div>
+		</div>
+		<div class="row icon_boxes_row">
+
+			<!-- Icon Box -->
+			<div class="col-xl-4 col-lg-6">
+				<div class="icon_box">
+					<div class="icon_box_title_container d-flex flex-row align-items-center justify-content-start">
+						<div class="icon_box_icon"><img src="images/icon_1.svg" alt=""></div>
+						<div class="icon_box_title">Cardiology</div>
+					</div>
+					<div class="icon_box_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lorem maximus malesuada lorem maximus mauris.</div>
+				</div>
+			</div>
+
+			<!-- Icon Box -->
+			<div class="col-xl-4 col-lg-6">
+				<div class="icon_box">
+					<div class="icon_box_title_container d-flex flex-row align-items-center justify-content-start">
+						<div class="icon_box_icon"><img src="images/icon_2.svg" alt=""></div>
+						<div class="icon_box_title">Gastroenterology</div>
+					</div>
+					<div class="icon_box_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lorem maximus malesuada lorem maximus mauris.</div>
+				</div>
+			</div>
+
+			<!-- Icon Box -->
+			<div class="col-xl-4 col-lg-6">
+				<div class="icon_box">
+					<div class="icon_box_title_container d-flex flex-row align-items-center justify-content-start">
+						<div class="icon_box_icon"><img src="images/icon_3.svg" alt=""></div>
+						<div class="icon_box_title">Medical Lab</div>
+					</div>
+					<div class="icon_box_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lorem maximus malesuada lorem maximus mauris.</div>
+				</div>
+			</div>
+
+			<!-- Icon Box -->
+			<div class="col-xl-4 col-lg-6">
+				<div class="icon_box">
+					<div class="icon_box_title_container d-flex flex-row align-items-center justify-content-start">
+						<div class="icon_box_icon"><img src="images/icon_4.svg" alt=""></div>
+						<div class="icon_box_title">Dental Care</div>
+					</div>
+					<div class="icon_box_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lorem maximus malesuada lorem maximus mauris.</div>
+				</div>
+			</div>
+
+			<!-- Icon Box -->
+			<div class="col-xl-4 col-lg-6">
+				<div class="icon_box">
+					<div class="icon_box_title_container d-flex flex-row align-items-center justify-content-start">
+						<div class="icon_box_icon"><img src="images/icon_5.svg" alt=""></div>
+						<div class="icon_box_title">Surgery</div>
+					</div>
+					<div class="icon_box_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lorem maximus malesuada lorem maximus mauris.</div>
+				</div>
+			</div>
+
+			<!-- Icon Box -->
+			<div class="col-xl-4 col-lg-6">
+				<div class="icon_box">
+					<div class="icon_box_title_container d-flex flex-row align-items-center justify-content-start">
+						<div class="icon_box_icon"><img src="images/icon_6.svg" alt=""></div>
+						<div class="icon_box_title">Neurology</div>
+					</div>
+					<div class="icon_box_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lorem maximus malesuada lorem maximus mauris.</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col">
+				<div class="button services_button ml-auto mr-auto"><a href="#"><span>read more</span><span>read more</span></a></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+require_once("footer.php");
+?>

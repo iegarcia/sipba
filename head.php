934785169c122fd9ca4ca41a678dac55cb340e51
Ignacio Garcia <nachogarcia003@gmail.com>
2020-08-28 20:04:21 -0300

    Marketing updates

--- head.php
+++ head.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html hreflang="es">
+<html lang="es">
 
 <head>
   <title>Servicio Integral de Psicologia Buenos Aires</title>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="keywords" content="salud mental, terapia, psicologo, psicologa, tratamiento psicologico, tcc, analisis, pedagogia, diagnosticos, cognitivo">
+  <meta name="keywords" content="ansiedad,terapia,tratamiento ansiedad,terapia cognitiva conductual,estres,Psicologia,online,terapia online,videollamada,cognitivo conductual,evaluación neuropsicológica,psicopedagogía,796,psicologa, online,psicólogo on line,psicoanálisis,psicología infantil,Neurodesarrollo,Estimulación cognitiva,Rehabilitación,neurocognitiva,Demencias,autismo,depresión,duelo,fobia, miedo, ataque,de pánico,ansiedad,social,autoestima,problemas de los aprendizajes,pareja,familia,padres,dislexia,discalculia,déficit de atención,memoria,atención,problemas de conducta,emociones,esquizofrenia,alimentación,suicidio,coeficiente,intelectual,apto,psicológico,niños,adultos,adolescentes">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Psicología on line. Terapia on line. Hablá con un profesional ya donde sea que te encuentres. Terapia cognitivo conductual. Psicoanálisis. Neuropsicología. Ansiedad. Depresión.">
   <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/db2580de36.js"></script>
@@ -21,7 +22,7 @@
           <div class="col">
             <div class="header_top_content d-flex flex-row align-items-center justify-content-start">
               <div class="logo">
-                <a href="index.php"><img src="img/logo.png" alt="logo"></a>
+                <a href="index.php" rel="canonical"><img src="img/logo.png" alt="logo"></a>
               </div>
               <div class="header_top_extra d-flex flex-row align-items-center justify-content-start ml-auto">
                 <div class="header_top_nav">
